eb9f70e559590a980fb61463a1e5dc225c0b4510
Koigor97 <turaysamuel101@yahoo.com>
2023-09-07 15:37:51 +0200

    Update index.ts

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -30,10 +30,52 @@
 // console.log(divideByTwo("pizza")); // NaN
 
 // A better way would be
-const divideByTwo = function (num: unknown) {
-  if (typeof num === "number") return Math.floor(num / 2);
-  else return "This function takes a number. Please enter a valid number";
-};
+// const divideByTwo = function (num: unknown) {
+//   if (typeof num === "number") return Math.floor(num / 2);
+//   else return "This function takes a number. Please enter a valid number";
+// };
 
-console.log(divideByTwo(32)); // 16
-console.log(divideByTwo("pizza")); // This function takes a number. Please enter a valid number
+// console.log(divideByTwo(32)); // 16
+// console.log(divideByTwo("pizza")); // This function takes a number. Please enter a valid number
+
+// BOOLEAN type
+// let booleanType: boolean;
+// booleanType = true;
+// console.log(booleanType);
+
+// booleanType = 1 > 0;
+// console.log(booleanType);
+
+// NUMBER type;
+// const num: number = 27;
+// const total: number = num * 28;
+
+// console.log(num);
+// console.log(total);
+
+// BIGINT type
+// const bigInt = 834624515n; // the javaScript way
+// const maximumInt = Number.MAX_SAFE_INTEGER;
+// console.log(maximumInt); //9007199254740991
+// console.log(maximumInt + 1); //9007199254740992
+// console.log(maximumInt + 2); //9007199254740992
+
+//OBJECT type
+// JavaScript way
+// const job = {
+//   name: "Software Developer",
+//   salaryPerMonth: 10_000,
+// };
+
+// const country: {
+//   name: string;
+//   population: number;
+//   cities: [string, string, string, string];
+// } = {
+//   name: "South Africa",
+//   population: 59_000_039,
+//   cities: ["Johannesburg", "Pretoria", "Cape Town", "Durban"],
+// };
+
+// console.log(job);
+// console.log(country);

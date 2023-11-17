3261e04717b0dc6c2d669f30a7fb5810e49051fb
Koigor97 <turaysamuel101@yahoo.com>
2023-11-17 11:35:50 +0200

    add new folder typescript compiler

---
+++ typescript-compiler/dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log("test");
+const person = "Koigor";
+const myGf = "Sarah Alpha";
+const sayHello = function (name) {
+    return `Hello ${name}`;
+};
+console.log(sayHello(myGf));
+console.log(sayHello(person));
+//# sourceMappingURL=app.js.map
---
+++ typescript-compiler/src/app.ts
@@ -0,0 +1,11 @@
+console.log("test");
+
+const person = "Koigor";
+const myGf = "Sarah Alpha";
+
+const sayHello = function (name: string): string {
+  return `Hello ${name}`;
+};
+
+console.log(sayHello(myGf));
+console.log(sayHello(person));
